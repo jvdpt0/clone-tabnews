2dff6e0222b19c56292fb7883b2f3f9c6c899838
Joao Victor <jvdpt0@gmail.com>
2026-02-20 13:14:13 -0300

    adiciona configuração de ssl

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
-  console.log("Database credentials:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -28,3 +23,13 @@ async function query(queryObject) {
 }
 
 export default { query };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
