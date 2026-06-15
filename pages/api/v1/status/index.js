5340f3f1995f5a8096f77d7c32b37d58d66868ab
Joao Victor <jvdpt0@gmail.com>
2026-06-15 15:59:39 -0300

    adiciona /api/v1/migrations endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -9,9 +9,11 @@ async function status(request, response) {
   const maxConnectionsResult = await database.query("SHOW max_connections;");
   const maxConnections = maxConnectionsResult.rows[0].max_connections;
 
-  const openConnectionsResult = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const openConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
   const openConnections = openConnectionsResult.rows[0].count;
 
   response.status(200).json({
